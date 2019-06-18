9982b67b7e843d4368cf76f7cae0079f9f081b49
MegiBegi <ms.magdalena.kozlowska@gmail.com>
2019-06-18 18:22:38 +0200

    tutorial resp api django done! it works

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from shop.restAPI import views
+from restAPI import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
--- restAPI/views.py
+++ restAPI/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
-
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from shop.restAPI.serializers import  UserSerializer, GroupSerializer
+from restAPI.serializers import  UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
